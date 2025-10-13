8d66bb8b871ecb59fdbfa4933fae794971f4b1ad
Evgeniy Doroshenko <kraspy@yandex.ru>
2025-10-13 20:44:03 +0700

    chore: [Practice] add info about str -> bytes

--- practice/python/strings.py
+++ practice/python/strings.py
@@ -3,3 +3,12 @@ for attr in str.__dict__:
         print(attr)
 
 print(f'{1000.125:.>+#010,.2f}')
+
+
+print(
+    ord('A'),
+    chr(65),
+    'A'.encode(encoding='utf-8'),
+    b'\x41'.decode(encoding='utf-8'),
+    sep='\n',
+)
