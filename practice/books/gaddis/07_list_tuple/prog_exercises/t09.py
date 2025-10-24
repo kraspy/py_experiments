55d70b119e9b65502047d0b37876d93f16e0e29a
Evgeniy Doroshenko <kraspy@yandex.ru>
2025-10-25 00:04:18 +0700

    chore: Add solved task

---
+++ practice/books/gaddis/07_list_tuple/prog_exercises/t09.py
@@ -0,0 +1,37 @@
+# Данные о населении. Среди исходного кода главы 7 вы найдете файл USPopulation.txt.
+# В нем хранятся данные о среднегодовой численности населения США в тысячах с 1950
+# по 1990 год. Первая строка в файле содержит численность населения в 1950 году, вторая
+# строка - численность населения в 1951 году и т. д.
+# Напишите программу, которая считывает содержимое файла в список. Программа должна
+# показать приведенные ниже данные:
+# • среднегодовое изменение численности населения в течение указанного периода времени;
+# • год с наибольшим увеличением численности населения в течение указанного периода
+# времени;
+# • год с наименьшим увеличением численности населения в течение указанного периода
+# времени.
+
+with open('USPopulation.txt') as f:
+    population_list = [int(p.strip()) for p in f.readlines()]
+
+changes_population = [
+    population_list[i + 1] - population_list[i] for i in range(len(population_list) - 1)
+]
+
+
+print('Статистика по населению США с 1950 по 1990 год', end='\n\n')
+print(
+    f'Среднегодовое изменение населения: {round(sum(changes_population) / len(changes_population))}'
+)
+
+max_population_index = changes_population.index(max(changes_population))
+year_of_max_population = 1950 + max_population_index
+
+print(
+    f'Наибольшее увеличение было в {year_of_max_population} году ({changes_population[max_population_index]})'
+)
+
+min_population_index = changes_population.index(min(changes_population))
+year_of_min_population = 1950 + min_population_index
+print(
+    f'Наименьшее увеличение было в {year_of_min_population} году ({changes_population[min_population_index]})'
+)
