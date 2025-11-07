6b5aa670e34ac52be644e5447c28a3fd0dc42519
Evgeniy Doroshenko <kraspy@yandex.ru>
2025-11-07 19:32:48 +0700

    refactor: optimize approaches

--- practice/some_tasks/py_timeit.py
+++ practice/some_tasks/py_timeit.py
@@ -1,39 +1,24 @@
 import timeit
 import time
 
+approaches = (
+    ('f-string', "f'{name}'"),
+    ('format()', "'{name}'.format(name=name)"),
+    ('printf %:', "'%s' % name"),
+)
 
-NUM_ITERATIONS = 1_000_000
-PRECISION = '.3f'
+print('*' * 10 + 'timeit' + '*' * 10)
+for name, stmt in approaches:
+    result = timeit.timeit(stmt, number=10000, setup="name = 'Evgeniy'")
+    print(f'{name}: {result}s')
+print('*' * 10 + 'timeit' + '*' * 10, end='\n')
 
-some_var = 'Hello'
-
-
-def fstring_format():
-    f'{some_var}'
-
-
-def str_format():
-    '{0}'.format(some_var)
-
-
-def printf():
-    '%s' % (some_var)
-
-
-print(f'f-string: {timeit.timeit(fstring_format, number=NUM_ITERATIONS):{PRECISION}}s')
-print(f'format(): {timeit.timeit(str_format, number=NUM_ITERATIONS):{PRECISION}}s')
-print(f'printf %: {timeit.timeit(printf, number=NUM_ITERATIONS):{PRECISION}}s')
-
-
-print(timeit.repeat(fstring_format, repeat=10, number=NUM_ITERATIONS))
+for name, stmt in approaches:
+    result = timeit.repeat(stmt, repeat=5, setup="name = 'Evgeniy'")
+    print(f'{name}: {result}')
 
 timer = timeit.Timer('len(x)', setup='x = [i for i in range(10_000_000)]')
 
 print(timer.timeit(number=1000000))
 
 print(timer.autorange())
-
-
-print(f'default_timer: {timeit.default_timer}')
-print(f'default_timer(): {timeit.default_timer()}')
-print(f'time.perf_counter: {time.perf_counter()}')
