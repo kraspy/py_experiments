5fa19361342f38e59835e4bd37c1fff6e7d70807
Evgeniy Doroshenko <kraspy@yandex.ru>
2025-11-19 23:55:27 +0700

    chore: Add basic classes examples

--- practice/oop/basic.py
+++ practice/oop/basic.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from typing import Protocol
+from typing import Any, Protocol
 
 
 # Abstract Class
@@ -72,3 +72,57 @@ with CtxManager() as cm:
     print(cm)
 
 print(cm)
+
+
+# get, set, del
+class TempClass:
+    def __getattribute__(self, name: str) -> Any:
+        print(f'__getattribute__: {name}')
+        return super().__getattribute__(name)
+
+    def __getattr__(self, name):
+        print(f'__getattr__: {name}')
+        return None
+
+    def __setattr__(self, name: str, value: Any) -> None:
+        print(f'__setattr__: {name}={value}')
+        super().__setattr__(name, value)
+
+
+tc = TempClass()
+tc.a = 1
+
+print(tc.a)
+print(tc.b)
+
+
+# Descriptors
+class Descriptor:
+    def __set_name__(self, owner, name):
+        print(f'Set Name: _{name} ({owner.__name__})')
+        return f'_{name}'
+
+    def __get__(self, instance, owner):
+        print(f'get: {instance=} / {owner=}')
+        return instance._descriptor
+
+    def __set__(self, instance, value):
+        print(f'set: {instance=} / {value=}')
+        instance._descriptor = value
+
+    def __delete__(self, instance):
+        print(f'del: {instance=}')
+        del instance._descriptor
+
+
+class MyClass:
+    descriptor = Descriptor()
+
+
+mc = MyClass()
+
+mc.descriptor = 1
+
+print(MyClass.__dict__, mc.__dict__, sep='\n\n')
+print(mc.descriptor)
+del mc.descriptor
