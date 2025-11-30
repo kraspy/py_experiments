9b99e2d81bf0b427c53af84a287522ab65d9a02f
Evgeniy Doroshenko <kraspy@yandex.ru>
2025-11-30 17:08:03 +0700

    chore: Add solved task

---
+++ practice/books/gaddis/12_recursion/algorithms/t01.py
@@ -0,0 +1,14 @@
+def main():
+    num = 0
+    show_me(num)
+
+
+def show_me(arg):
+    if arg < 10:
+        show_me(arg + 1)
+    else:
+        print(arg)
+
+
+if __name__ == '__main__':
+    main()
